7d437a1783a77a7f3a789e40650264339a80daca
Serhii Kupriienko <skupriienko@anaconda.com>
2022-10-13 12:57:58 +0300

    Copy leptonica headers

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,19 @@
 :: cmd
 
+copy %LIBRARY_LIB%\leptonica\allheaders.h %LIBRARY_LIB%\allheaders.h
+
 echo "Building %PKG_NAME%."
 cd tesseract
 if errorlevel 1 exit /b 1
+
+
 
 :: Isolate the build.
 mkdir Build-%PKG_NAME%
 cd Build-%PKG_NAME%
 if errorlevel 1 exit /b 1
+
+
 
 :: Generate the build files.
 echo "Generating the build files..."
@@ -17,7 +23,7 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D Leptonica_DIR=%LIBRARY_PREFIX%\leptonica ^
+      -D Leptonica_DIR=%LIBRARY_PREFIX% ^
       -D SW_BUILD=OFF ^
       -D BUILD_TRAINING_TOOLS=OFF ^
       -D BUILD_SHARED_LIBS=ON ^
